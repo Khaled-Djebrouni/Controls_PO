ce6b26297b63f6ade31cf06b038c5612ea3e482e
Khaled-Djebrouni <141644823+Khaled-Djebrouni@users.noreply.github.com>
2024-02-12 09:32:55 +0100

    Create purchaseOrder_sheets.sql
    Initial code

---
+++ purchaseOrder_sheets.sql
@@ -0,0 +1,23 @@
+USE [sdbPILOT3_MM]
+GO
+
+/****** Object:  Table [dbo].[purchaseOrder_sheets]    Script Date: 11.02.2024 8:11:50 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[purchaseOrder_sheets](
+	[name] [nvarchar](100) NULL
+) ON [PRIMARY]
+GO
+
+
+-- INSERT INTO [dbo].[purchaseOrder_sheets] ([name]) VALUES ('PurchaseOrder_Entete');
+-- INSERT INTO [dbo].[purchaseOrder_sheets] ([name]) VALUES ('PurchaseOrder_Conditionsentete');
+-- INSERT INTO [dbo].[purchaseOrder_sheets] ([name]) VALUES ('PurchaseOrder_Textesentete');
+-- INSERT INTO [dbo].[purchaseOrder_sheets] ([name]) VALUES ('PurchaseOrder_Postes');
+-- INSERT INTO [dbo].[purchaseOrder_sheets] ([name]) VALUES ('PurchaseOrder_ConditionsPoste');
+-- INSERT INTO [dbo].[purchaseOrder_sheets] ([name]) VALUES ('PurchaseOrder_Lignesservices');
+-- INSERT INTO [dbo].[purchaseOrder_sheets] ([name]) VALUES ('PurchaseOrder_imputation');
