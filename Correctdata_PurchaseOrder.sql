10da8db8acd136768343a0a2942fcdc823811d3c
Khaled-Djebrouni <141644823+Khaled-Djebrouni@users.noreply.github.com>
2024-02-15 15:42:40 +0100

    Update Correctdata_PurchaseOrder.sql

--- Correctdata_PurchaseOrder.sql
+++ Correctdata_PurchaseOrder.sql
@@ -1,18 +1,19 @@
 USE [sdbPILOT3_MM]
 GO
 
-/****** Object:  StoredProcedure [dbo].[Correctdata_PurchaseOrder]    Script Date: 09.02.2024 19:13:11 ******/
+/****** Object:  StoredProcedure [dbo].[Correctdata_PurchaseOrder]    Script Date: 15.02.2024 15:29:23 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
 
+
 ALTER PROCEDURE [dbo].[Correctdata_PurchaseOrder]
 AS
 BEGIN
-	DECLARE @UPDATE_PO_P_Type NVARCHAR(MAX)
-	DECLARE @UPDATE_LS_MATKL_NULL NVARCHAR(MAX)
+	DECLARE @UPDATE_PO_P_Type NVARCHAR(MAX);
+	DECLARE @UPDATE_LS_MATKL_NULL NVARCHAR(MAX);
 
 	SET @UPDATE_PO_P_Type			= N'UPDATE PurchaseOrder_Postes
 										SET MENGE = ''1'',
@@ -29,10 +30,21 @@ BEGIN
 																		FROM
 																		contrôle_PurchaseOrder_Lignesservices_erreur
 																		WHERE contrôle_MATKL like ''%NOK%'' );';
-	PRINT @UPDATE_PO_P_Type;
-	PRINT @UPDATE_LS_MATKL_NULL;
-    EXEC sp_executesql @UPDATE_PO_P_Type
-    EXEC sp_executesql @UPDATE_LS_MATKL_NULL
-	
+    EXEC sp_executesql @UPDATE_PO_P_Type;
+    EXEC sp_executesql @UPDATE_LS_MATKL_NULL;
+
+	IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'PurchaseOrder_LignesServices' AND COLUMN_NAME='FORMELNR')
+	BEGIN
+		ALTER TABLE PurchaseOrder_LignesServices ADD FORMELNR NVARCHAR(MAX);
+	END;
+
+	;WITH CTE AS (
+    SELECT FORMELNR,
+           ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS SequenceNumber
+    FROM PurchaseOrder_LignesServices
+	)
+	UPDATE CTE
+	SET FORMELNR = CAST(SequenceNumber AS NVARCHAR(MAX));
+
 END
 GO
