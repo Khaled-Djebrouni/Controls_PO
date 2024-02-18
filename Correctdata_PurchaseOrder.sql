2a20152548f7e31c8c9c6c047afbcb331f5504d5
Khaled-Djebrouni <141644823+Khaled-Djebrouni@users.noreply.github.com>
2024-02-18 08:16:45 +0100

    Update Correctdata_PurchaseOrder.sql

--- Correctdata_PurchaseOrder.sql
+++ Correctdata_PurchaseOrder.sql
@@ -12,41 +12,41 @@ GO
 ALTER PROCEDURE [dbo].[Correctdata_PurchaseOrder]
 AS
 BEGIN
-	DECLARE @UPDATE_PO_P_Type NVARCHAR(MAX);
-	DECLARE @UPDATE_LS_MATKL_NULL NVARCHAR(MAX);
+    DECLARE @UPDATE_PO_P_Type NVARCHAR(MAX);
+    DECLARE @UPDATE_LS_MATKL_NULL NVARCHAR(MAX);
 
-	SET @UPDATE_PO_P_Type			= N'UPDATE PurchaseOrder_Postes
-										SET MENGE = ''1'',
-											MEINS = ''UO'',
-											NETPR = NULL
-										WHERE PSTYP = ''P'' AND
-										(	MENGE <> ''1'' OR
-											MEINS <> ''UO'' OR
-											NETPR IS NOT NULL	) ;';
+    SET @UPDATE_PO_P_Type           = N'UPDATE PurchaseOrder_Postes
+                                        SET MENGE = ''1'',
+                                            MEINS = ''UO'',
+                                            NETPR = NULL
+                                        WHERE PSTYP = ''P'' AND
+                                        (   MENGE <> ''1'' OR
+                                            MEINS <> ''UO'' OR
+                                            NETPR IS NOT NULL   ) ;';
 
-	SET @UPDATE_LS_MATKL_NULL = N'UPDATE PurchaseOrder_LignesServices
+    SET @UPDATE_LS_MATKL_NULL = N'UPDATE PurchaseOrder_LignesServices
                                 SET MATKL = NULL
-                                WHERE CONCAT(EBELN, EBELP, MATKL) IN (	SELECT DISTINCT CONCAT(EBELN, EBELP, MATKL)
-																		FROM
-																		contrôle_PurchaseOrder_Lignesservices_erreur
-																		WHERE contrôle_MATKL like ''%NOK%'' );';
+                                WHERE CONCAT(EBELN, EBELP, MATKL) IN (  SELECT DISTINCT CONCAT(EBELN, EBELP, MATKL)
+                                                                        FROM
+                                                                        contrôle_PurchaseOrder_Lignesservices_erreur
+                                                                        WHERE contrôle_MATKL like ''%NOK%'' );';
     EXEC sp_executesql @UPDATE_PO_P_Type;
     EXEC sp_executesql @UPDATE_LS_MATKL_NULL;
 
 
-	IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'PurchaseOrder_LignesServices' AND COLUMN_NAME='FORMELNR')
-	BEGIN
-		ALTER TABLE PurchaseOrder_LignesServices ADD FORMELNR NVARCHAR(MAX);
-	END;
+    IF NOT EXISTS(SELECT 1 FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'PurchaseOrder_LignesServices' AND COLUMN_NAME='FORMELNR')
+    BEGIN
+        ALTER TABLE PurchaseOrder_LignesServices ADD FORMELNR NVARCHAR(MAX);
+    END;
 
 
-	;WITH CTE AS (
+    ;WITH CTE AS (
     SELECT FORMELNR,
            ROW_NUMBER() OVER (ORDER BY (SELECT NULL)) AS SequenceNumber
     FROM PurchaseOrder_LignesServices
-	)
-	UPDATE CTE
-	SET FORMELNR = CAST(SequenceNumber AS NVARCHAR(MAX));
+    )
+    UPDATE CTE
+    SET FORMELNR = CAST(SequenceNumber AS NVARCHAR(MAX));
 
 
 END
